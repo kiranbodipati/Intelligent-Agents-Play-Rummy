5f036084612ab08b2a4ab85f243110d91bf238f3
Varun2101 <iyengar.varun@gmail.com>
2021-03-02 16:45:57 +0800

    Added game state initialization

--- control_classes.py
+++ control_classes.py
@@ -1,7 +1,30 @@
 from entity_classes import *
 
 class GameMgr:
-    pass
+    def __init__(self, seed=None):
+        self.deck = Deck()
+        self.discardPile = DiscardPile()
+        self.hand1 = Hand()
+        self.hand2 = Hand()
+        
+        # initializes game state
+        self.deck.shuffle(seed)
+        self.deck.joker = self.deck.draw()
+        for _ in range(13):
+            self.hand1.draw(self.deck.draw())
+            self.hand2.draw(self.deck.draw())
+        self.discardPile.discard(self.deck.draw())
+
+        print("Hand 1:", self.hand1)
+        print()
+        print("Hand 2:", self.hand2)
+        print()
+        print(self.discardPile)
+        print()
+        print(self.deck)
+        print("Joker:", self.deck.joker)
+        print("Length of deck:", len(self.deck.cards))
+        print()
 
 class Player:
     pass
@@ -11,3 +34,7 @@ class BasicAgent(Player):
 
 class AdvancedAgent(Player):
     pass
+
+
+if __name__ == "__main__":
+    GameMgr(123)
--- entity_classes.py
+++ entity_classes.py
@@ -1,25 +1,50 @@
 from collections import deque
+import numpy as np
 import random
+
+suitDict = {"S": 0, "H": 1, "C": 2, "D":3}
 
 class Card:
     def __init__(self, val, st):
         self.value = val
         self.suit = st
+    
+    def __repr__(self) -> str:
+        return "Card()"
+    
+    def __str__(self) -> str:
+        strVal = ["A"] + [str(x) for x in range(2, 11)] + ["J", "Q", "K"]
+        if self.value != -1:
+            return self.suit + strVal[self.value - 1]
+        else:
+            return "JKR"
 
 class Deck:
     def __init__(self):
         self.cards = []
         # each card is represented by a letter for suit + number for value. A is 1, J, Q, K are 11, 12, 13 respectively
-        for s in ["S", "H", "C", "D"]:
+        for s in suitDict.keys():
             for v in range(1, 14):
                 self.cards.append(Card(v, s))
         self.cards.append(Card(-1, "jk"))
         self.cards.append(Card(-1, "jk"))
+        self.joker = None
     
-    def shuffle(self):
+    def __repr__(self) -> str:
+        return "Deck()"
+    
+    def __str__(self) -> str:
+        output = "Deck: "
+        for card in self.cards:
+            output += str(card) + "    "
+        return output
+    
+    def shuffle(self, seed):
+        if seed:
+            random.seed(seed)
         random.shuffle(self.cards)
     
-    def deal(self):
+    def draw(self):
         return self.cards.pop()
     
     def returnToDeck(self, card):
@@ -28,6 +53,15 @@ class Deck:
 class DiscardPile:
     def __init__(self):
         self.cards = deque()
+    
+    def __repr__(self) -> str:
+        return "DiscardPile()"
+    
+    def __str__(self) -> str:
+        output = "Discard Pile: "
+        for card in self.cards:
+            output += str(card) + "    "
+        return output
     
     def draw(self):
         return self.cards.pop()
@@ -38,12 +72,34 @@ class DiscardPile:
 class Hand:
     def __init__(self):
         self.cards = []
+        self.cardMatrix = np.zeros((4,13))
+        self.jokers = 0
+    
+    def __repr__(self) -> str:
+        return "Hand()"
+    
+    def __str__(self) -> str:
+        output = ""
+        for card in self.cards:
+            output += str(card) + "    "
+        return output
     
     def draw(self, card):
         self.cards.append(card)
+        if card.value != -1:
+            self.cardMatrix[suitDict[card.suit]][card.value-1] = 1
+        else:
+            self.jokers += 1
     
     def discard(self, val):
         for index, card in enumerate(self.cards):
             if card.value == val:
+                if val != -1:
+                    self.cardMatrix[suitDict[card.suit]][card.value-1] = 0
+                else:
+                    self.jokers -= 1
                 return self.cards.pop(index)
         return "error"
+    
+    def checkMelds(self):  # only checks for win state for now, returns bool if game is won
+        pass
