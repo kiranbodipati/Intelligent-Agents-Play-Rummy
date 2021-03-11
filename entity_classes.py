0ba4191c80d51911531f056236e122da09b8c9bf
Varun2101 <iyengar.varun@gmail.com>
2021-03-11 17:24:38 +0800

    Check melds framework

--- entity_classes.py
+++ entity_classes.py
@@ -4,10 +4,35 @@ import random
 
 suitDict = {"S": 0, "H": 1, "C": 2, "D":3}
 
+# Helper functions for matrix representation of cards
+
+# Gets index of first occuring card in a hand (row-wise)
+def getFirstCard(matrix, shp):
+    for i in range(shp[0]):
+        for j in range(shp[1]):
+            if matrix[i][j]:
+                return (i, j)
+
+# Gets all indexes of same value for same-value melds
+def getSameValue(matrix, index):
+    j = index[1]
+    result = []
+    for i in range(4):
+        if i != index[0] and matrix[i][j]:
+            result.append(i)
+    return result
+
+# Entity classes
+
 class Card:
     def __init__(self, val, st):
-        self.value = val
+        self.value = val  # actual number on card
         self.suit = st
+        self.points = val  # point value of card
+        if val == -1:  # joker cards have no points
+            self.points = 0
+        elif val == 1 or val > 10:  # ace and face cards
+            self.points = 10
     
     def __repr__(self) -> str:
         return "Card()"
@@ -26,8 +51,8 @@ class Deck:
         for s in suitDict.keys():
             for v in range(1, 14):
                 self.cards.append(Card(v, s))
-        self.cards.append(Card(-1, "jk"))
-        self.cards.append(Card(-1, "jk"))
+        self.cards.append(Card(-1, "JK1"))
+        self.cards.append(Card(-1, "JK2"))
         self.joker = None
     
     def __repr__(self) -> str:
@@ -100,6 +125,80 @@ class Hand:
                     self.jokers -= 1
                 return self.cards.pop(index)
         return "error"
-    
-    def checkMelds(self):  # only checks for win state for now, returns bool if game is won
-        pass
+
+    def checkMelds(self, matrix=None, jkr=None):  # only checks for win state for now, returns bool if game is won
+        if matrix==None:
+            matrix = self.cardMatrix.copy()
+        if jkr==None:
+            jkr = self.jokers.copy()
+        # Recursive function
+        # find first 1 in matrix, check all melds including chances+jokers (reduce num of jokers in this case) using this card
+        # for each meld, recursive call checkMelds with new matrix, without the cards in the meld being considered
+        # if no chances => return false, backtrack one step
+        # base case => matrix is all 0s, return true
+        # end of recursion case => if all melds lead to false, return false
+
+        # Base case:
+        if np.all(matrix==0):
+            return True
+        
+        # Recursive case
+        # search for straight melds on right side only, prevents overlap
+        shp = np.shape(matrix)
+        i, j = getFirstCard(matrix, shp)
+        if j == 12:  # king
+            right1 = right2 = right3 = (False, (i,j))
+        elif j == 11:  # queen
+            right1 = (bool(matrix[i][j+1]), (i, j+1))
+            right2 = (bool(matrix[i][0]), (i, 0))
+            right3 = False
+        elif j == 10:  # jack:
+            right1 = (bool(matrix[i][j+1]), (i, j+1))
+            right2 = (bool(matrix[i][j+2]), (i, j+2))
+            right3 = (bool(matrix[i][0]), (i, 0))
+        else:
+            right1 = (bool(matrix[i][j+1]), (i, j+1))
+            right2 = (bool(matrix[i][j+2]), (i, j+2))
+            right3 = (bool(matrix[i][j+3]), (i, j+3))
+        
+        melds = []
+        # no elif anywhere in case the other card needs to be used for something; considers all cases
+
+        # straight melds
+        # pure highest in order so it's considered first if possible
+        if right1[0] and right2[0]:
+            # add to melds here
+            if right3[0]:
+                # add to melds here
+                pass
+            if jkr:
+                # add to melds here
+                pass
+        if right1[0] and jkr:
+            # add to melds here
+            if right3[0]:
+                # add to melds here
+                pass
+        if right2[0] and jkr:
+            # add to melds here
+            if right3[0]:
+                # add to melds here
+                pass
+        if right3[0] and jkr==2:
+            # add to melds here
+            pass
+        if jkr==2:
+            # add to melds here
+            pass
+
+        # same value melds
+        j_vals = getSameValue(matrix, (i, j))
+        if len(j_vals) == 3:
+            # add to melds here
+            pass
+        if len(j_vals) == 2:
+            # add to melds here
+            pass
+        if jkr:
+            # add to melds here
+            pass
