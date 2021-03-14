ada8f42854fbc9c208a40e488a35c651c79dbcf1
Kiran Bodipati <kiranbodipati@gmail.com>
2021-03-14 16:07:01 +0800

    removed hardcoding, replaced with array

--- control_classes.py
+++ control_classes.py
@@ -5,16 +5,16 @@ class GameMgr:
     def __init__(self, seed=None):
         self.deck = Deck()
         self.discardPile = DiscardPile()
-        self.Player1 = Player()
-        self.Player2 = Player()
+        # self.Player1 = Player()
+        # self.Player2 = Player()
         # initializes game state
-        
+        self.Players = [Player(), Player()]
         
         self.DealGame(seed)
 
-        print("Hand 1:", self.Player1.hand)
+        print("Hand 1:", self.Players[0].hand)
         print()
-        print("Hand 2:", self.Player2.hand)
+        print("Hand 2:", self.Players[1].hand)
         print()
         print(self.discardPile)
         print()
@@ -29,67 +29,47 @@ class GameMgr:
         # initializes game state
         self.deck.shuffle(seed)
         self.deck.joker = self.deck.draw()
-        self.Player1.hand.setJoker(self.deck.joker.value)
-        self.Player2.hand.setJoker(self.deck.joker.value)
+        self.Players[0].hand.setJoker(self.deck.joker.value)
+        self.Players[0].hand.setJoker(self.deck.joker.value)
         for _ in range(13):
-            self.Player1.hand.draw(self.deck.draw())
-            self.Player2.hand.draw(self.deck.draw())
+            self.Players[0].hand.draw(self.deck.draw())
+            self.Players[1].hand.draw(self.deck.draw())
         self.discardPile.discard(self.deck.draw())    
     
     def PlayPvP(self):
-        CurrentPlayer=random.choice([1,2])
+        CurrentPlayer=random.choice([0,1])
         self.turn=0
         while(True):
             self.Play(CurrentPlayer)
             self.turn+=1
-            if CurrentPlayer==1:
-                if self.Player1.hand.checkMelds()==True:
-                    break
-                CurrentPlayer=2
-            elif CurrentPlayer==2:
-                if self.Player2.hand.checkMelds()==True:
-                    break
+            if self.Players[CurrentPlayer].hand.checkMelds()==True:
+                break
+            if CurrentPlayer==0:
                 CurrentPlayer=1
+            elif CurrentPlayer==1:
+                CurrentPlayer=0
             print("Turn:",self.turn)
 
     
     def Play(self, CurrentPlayer):
-        print(int(CurrentPlayer), "\'s turn:")
-        if CurrentPlayer==1:
+        print(int(CurrentPlayer+1), "\'s turn:")
             
-            print("Hand:", self.Player1.hand)
-            print()
-            print("Where do you want to?\n Enter \n'D' to draw from Deck \n 'P' to draw from discard pile")
-            
-            loc=input()
-            if loc=='D':
-                self.Player1.hand.draw(self.deck.draw())
-            elif loc=='P':
-                self.Player1.hand.draw(self.discardPile.draw())
-            print("index: ", "".join([str(x)+"   " for x in range(0, len(self.Player1.hand.cards))]))
-            print("Hand:", self.Player1.hand)
-            print("enter index of card to disard:")
-            ind=int(input())
-            self.discardPile.discard(self.Player1.hand.discard(ind))
-            print("Hand:", self.Player1.hand)
+        print("Hand:", self.Players[CurrentPlayer].hand)
+        print()
+        print("Where do you want to?\nEnter \n'D' to draw from Deck \n'P' to draw from discard pile")
         
-        if CurrentPlayer==2:
-            
-            print("Hand:", self.Player2.hand)
-            print()
-            print("Where do you want to?\n Enter \n'D' to draw from Deck \n 'P' to draw from discard pile")
-            
-            loc=input()
-            if loc=='D':
-                self.Player2.hand.draw(self.deck.draw())
-            elif loc=='P':
-                self.Player2.hand.draw(self.discardPile.draw())
-            print("index: ", "".join([str(x)+"   " for x in range(0, len(self.Player1.hand.cards))]))
-            print("Hand:", self.Player2.hand)
-            print("enter index of card to disard:")
-            ind=int(input())
-            self.discardPile.discard(self.Player2.hand.discard(ind))
-            print("Hand:", self.Player2.hand)
+        loc=input()
+        if loc=='D':
+            self.Players[CurrentPlayer].hand.draw(self.deck.draw())
+        elif loc=='P':
+            self.Players[CurrentPlayer].hand.draw(self.discardPile.draw())
+        print("index: ", "".join([str(x)+"   " for x in range(0, len(self.Players[CurrentPlayer].hand.cards))]))
+        print("Hand:", self.Players[CurrentPlayer].hand)
+        print("enter index of card to disard:")
+        ind=int(input())
+        self.discardPile.discard(self.Players[CurrentPlayer].hand.discard(ind))
+        print("Hand:", self.Players[CurrentPlayer].hand)
+        
 
 class Player:
     def __init__(self):
